c21f076cf83d0a42d816a2b3e0435f34c3ba9fac
Shawn Mathew <shawn.mathew@gmail.com>
2016-09-22 18:14:06 -0400

    clean up work controller

--- public/src/js/controllers/allWorkctrl.js
+++ public/src/js/controllers/allWorkctrl.js
@@ -30,37 +30,11 @@ angular.module("portfolio")
 							.animate({
 								"opacity": 1								
 							}, 2500);		
-					});
-					
-
-					// $(".workItem__link img").each(function(indx){					
-					// 	var imgSrc = $(this).attr("data-img-src");
-					// 	$(this).attr( "src", imgSrc );
-					// 	$(this).on("load", function(){
-					// 		var d = new Date(),
-					// 			t = d.getTime();
-					// 		console.log( $(this).attr("src") + ": " + t );						
-
-					// 		$(this).parent()
-					// 			.delay(indx * 250)
-					// 			.animate({
-					// 				"opacity": 1								
-					// 			}, 500);
-					// 	});
-
-					// 	// var imgSrc = $(this).attr("data-img-src");
-					// 	// $(this).attr( "src", imgSrc );
-					// 	// $(this).parent()
-					// 	// 	.delay(indx * 250)
-					// 	// 	.animate({
-					// 	// 		"opacity": 1								
-					// 	// 	}, 500);
-					// });
-				});
-				
+					});				
+				});				
 			}; 
 
 			setTimeout(function(){
 				$scope.init();
-			}, 500);
+			}, 500);			
 	}]);
