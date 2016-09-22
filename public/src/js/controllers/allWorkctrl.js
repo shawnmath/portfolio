590be47fcbf3758b783a1540b3757ca84e04e8a4
Shawn Mathew <shawn.mathew@gmail.com>
2016-09-22 16:13:10 -0400

    work page img loading

--- public/src/js/controllers/allWorkctrl.js
+++ public/src/js/controllers/allWorkctrl.js
@@ -2,14 +2,45 @@
 
 angular.module("portfolio")
 	.controller("AllWorkCtrl", [
+		"$state",
+		"$rootScope",
 		"$scope", 
 		"GetAllWork", 
-		function($scope, GetAllWork){		
+		function($state, $rootScope, $scope, GetAllWork){		
 
 			GetAllWork.query(function(work){		
-				$scope.work = work;			
+				$scope.work = work;				
 			});
 			
-			//Limit work items to first 4 on homepage
+			// Limit work items to first 4 on homepage
 			$scope.limit = 4;
+
+			// angular.element( document.querySelectorAll(".workItem__link img") ).ready(function(){
+			// 	console.log("loaded..");
+				
+			// 	$scope.loadImgs();
+				
+			// });
+
+			// Fade In Imgs
+			$scope.init = function(){
+				$(document).ready(function(){
+					$(".workItem__link img").each(function(indx){					
+					var imgSrc = $(this).attr("data-img-src");
+					console.log(imgSrc);
+					$(this)
+						.delay(indx * 250)
+						.attr( "src", imgSrc )
+						//.removeClass("placeholder")
+						.animate({
+							"opacity": 0.4
+						}, 500);
+				});
+				});
+				
+			}; 
+
+			setTimeout(function(){
+				$scope.init();
+			}, 500);
 	}]);
