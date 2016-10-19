e1859c3db4db879de2fb5962402639ae09a795ce
Shawn Mathew <shawn.mathew@gmail.com>
2016-10-19 18:32:19 -0400

    edit html5Mode

--- public/src/js/routes/mainRoutes.js
+++ public/src/js/routes/mainRoutes.js
@@ -3,7 +3,7 @@
 angular.module("mainRoutes", ["ui.router"])
 	.config(["$stateProvider", "$locationProvider", "$urlRouterProvider", function($stateProvider, $locationProvider, $urlRouterProvider){
 		
-		$locationProvider.html5Mode();
+		$locationProvider.html5Mode(true);
 
 		$urlRouterProvider.otherwise("/");
 
